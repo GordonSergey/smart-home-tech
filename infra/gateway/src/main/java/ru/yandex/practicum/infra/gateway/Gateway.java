b36af21f33c3b982f45f8bcca2cecde0a67bd443
gordo <mrgordon1111@gmail.com>
2025-09-27 01:59:44 +0200

    Обновил gateway-приложение

--- infra/gateway/src/main/java/ru/yandex/practicum/infra/gateway/Gateway.java
+++ infra/gateway/src/main/java/ru/yandex/practicum/infra/gateway/Gateway.java
@@ -1,11 +1,11 @@
-package ru.yandex.practicum;
+package ru.yandex.practicum.infra.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@EnableConfigServer
 @SpringBootApplication
+@EnableDiscoveryClient
 public class Gateway {
     public static void main(String[] args) {
         SpringApplication.run(Gateway.class, args);
